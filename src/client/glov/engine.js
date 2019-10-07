055d5938c40e55fecc4541820ac61969e9971c90
Jimb Esser <wasteland@gmail.com>
2019-10-07 10:38:36 -0700

    Move PICO-8 palette out into separate util

--- src/client/glov/engine.js
+++ src/client/glov/engine.js
@@ -71,24 +71,6 @@ export let light_dir_ws = vec3(-1, -2, -3);
 export let font;
 export let app_state = null;
 export const border_color = vec4(0, 0, 0, 1);
-export const pico8_colors = [
-  vec4(0, 0, 0, 1),
-  vec4(0.114, 0.169, 0.326, 1),
-  vec4(0.494, 0.145, 0.326, 1),
-  vec4(0.000, 0.529, 0.328, 1),
-  vec4(0.671, 0.322, 0.212, 1),
-  vec4(0.373, 0.341, 0.310, 1),
-  vec4(0.761, 0.765, 0.780, 1),
-  vec4(1.000, 0.945, 0.910, 1),
-  vec4(1.000, 0.000, 0.302, 1),
-  vec4(1.000, 0.639, 0.000, 1),
-  vec4(1.000, 0.925, 0.153, 1),
-  vec4(0.000, 0.894, 0.212, 1),
-  vec4(0.161, 0.678, 1.000, 1),
-  vec4(0.514, 0.463, 0.612, 1),
-  vec4(1.000, 0.467, 0.659, 1),
-  vec4(1.000, 0.800, 0.667, 1),
-];
 
 export let fps_style = glov_font.style({
   outline_width: 2, outline_color: 0x00000080,
