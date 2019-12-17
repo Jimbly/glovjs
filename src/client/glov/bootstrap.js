77749e74487354a477ae3ca0d5660680709b2142
Jimb Esser <wasteland@gmail.com>
2019-12-17 12:27:59 -0800

    Better error/stack capture on FireFox

--- src/client/glov/bootstrap.js
+++ src/client/glov/bootstrap.js
@@ -6,7 +6,26 @@
 (function () {
   let debug = document.getElementById('debug');
   window.onerror = function (e, file, line, col, errorobj) {
-    let msg = errorobj && errorobj.stack || `${e}\n  at ${file}(${line}:${col})`;
+    let msg = `${e}\n  at ${file}(${line}:${col})`;
+    if (errorobj && errorobj.stack) {
+      msg = `${errorobj.stack}`;
+      if (errorobj.message) {
+        if (msg.indexOf(errorobj.message) === -1) {
+          msg = `${errorobj.message}\n${msg}`;
+        }
+      }
+      let origin = document.location.origin || '';
+      if (origin) {
+        if (origin.slice(-1) !== '/') {
+          origin += '/';
+        }
+        msg = msg.split(origin).join(''); // replace
+      }
+      // fixup weird Firefox weirdness
+      msg = msg.replace(/\[\d+\]</g, '') // remove funny [123] at start of stack lines
+        .replace(/<?\/<?/g, '/') // remove funny <s, they mess up people's copy and paste
+        .replace(/\n([^ ])/g, '\n  $1'); // add indentation if missing
+    }
     let show = true;
     if (window.glov_error_report) {
       show = window.glov_error_report(msg, file, line, col);
