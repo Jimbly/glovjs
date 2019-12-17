f7d2c8e693f7ded8079915bb04c451f1f233a883
Jimb Esser <wasteland@gmail.com>
2019-12-17 12:30:31 -0800

    Protect against window.onload firing more than once

--- src/client/app.js
+++ src/client/app.js
@@ -2,7 +2,12 @@
 
 // Startup code.
 
+let called_once = false;
 window.onload = function () {
+  if (called_once) {
+    return;
+  }
+  called_once = true;
   require('./glov/bootstrap.js');
   require('./main.js').main();
 };
