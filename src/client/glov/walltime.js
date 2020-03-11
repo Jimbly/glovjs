29b4c1b7d779f18c541684389e05083e76661116
Jimb Esser <wasteland@gmail.com>
2020-03-10 19:44:38 -0700

    Add walltime.seconds()

--- src/client/glov/walltime.js
+++ src/client/glov/walltime.js
@@ -1,3 +1,7 @@
+// Portions Copyright 2019 Jimb Esser (https://github.com/Jimbly/)
+// Released under MIT License: https://opensource.org/licenses/MIT
+
+const { floor } = Math;
 
 let offs = 0;
 function now() {
@@ -7,4 +11,8 @@ module.exports = exports = now;
 exports.now = now;
 exports.sync = function (server_time) {
   offs = server_time - Date.now();
+};
+exports.seconds = function () {
+  // Seconds since Jan 1st, 2020
+  return floor(now() / 1000) - 1577836800;
 };
