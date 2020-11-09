49c08d4daade57e85b1bca76d17a05ec648d4e70
Jimb Esser <wasteland@gmail.com>
2020-11-09 07:36:29 -0800

    Add additional browser identification

--- src/client/glov/browser.js
+++ src/client/glov/browser.js
@@ -1,5 +1,7 @@
 let ua = window.navigator.userAgent;
-export let is_ios = ua.match(/iPad/i) || ua.match(/iPhone/i);
+export let is_ios = !window.MSStream && ua.match(/iPad|iPhone|iPod/);
+export let is_windows_phone = ua.match(/windows phone/i);
+export let is_android = !is_windows_phone && ua.match(/android/i);
 export let is_webkit = ua.match(/WebKit/i);
 export let is_ios_safari = is_ios && is_webkit && !ua.match(/CriOS/i);
 
