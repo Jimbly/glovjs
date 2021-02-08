e48c20ed914296c0b9588ad40de1e14c22fa1a5e
Jimb Esser <wasteland@gmail.com>
2021-02-08 14:04:15 -0800

    Minimal test main.js

--- build/config.project.js
+++ build/config.project.js
@@ -1,31 +1,3 @@
 module.exports = function (config) {
-  config.bundles.push({
-    entrypoint: 'worker',
-    deps: 'worker_deps',
-    is_worker: true,
-    do_version: null,
-  });
-  config.extra_index.push({
-    name: 'multiplayer',
-    defines: {
-      PLATFORM: 'web',
-      ENV: 'multiplayer',
-    },
-    zip: false,
-  }, {
-    name: 'entity',
-    defines: {
-      PLATFORM: 'web',
-      ENV: 'entity',
-    },
-    zip: true,
-  });
-
-  // Spine support
-  // Note: Runtime requires a Spine license to use in any product.
-  config.client_fsdata.push(
-    'client/spine/**.atlas',
-    'client/spine/**.skel',
-    'client/spine/**.json',
-  );
+  // nothing; pure defaults
 };
