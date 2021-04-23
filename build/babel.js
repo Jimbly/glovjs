84d137179dcb1d3940bccd5218ea4f4c3b18924a
Jimb Esser <wasteland@gmail.com>
2021-04-23 14:18:17 -0700

    Refactor babel task into separate file

---
+++ build/babel.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const gb = require('glov-build');
+const path = require('path');
+const sourcemap = require('./sourcemap.js');
+
+module.exports = function (opts) {
+  opts = opts || {};
+  let babel_opts = {
+    sourceMap: true,
+    ...opts.babel,
+  };
+  let babel;
+  function babelInit(next) {
+    if (!babel) {
+      // eslint-disable-next-line global-require
+      babel = require('@babel/core');
+      // prime it for timing purposes
+      babel.transformSync('', {
+        filename: path.join(gb.getSourceRoot(), 'foo.js'),
+        filenameRelative: 'foo.js',
+        sourceFileName: 'foo.js',
+        ...babel_opts
+      });
+    }
+    next();
+  }
+  function babelTaskFunc(job, done) {
+    let source_file = job.getFile();
+    let source_code = source_file.contents.toString();
+    let result;
+    try {
+      result = babel.transformSync(source_code, {
+        // even if the file does not actually live in the source dir, treat it as such, for finding .babelrc files
+        filename: path.join(gb.getSourceRoot(), source_file.relative),
+        filenameRelative: source_file.relative,
+        sourceFileName: source_file.relative,
+        ...babel_opts
+      });
+    } catch (err) {
+      return void done(err);
+    }
+    assert.equal(typeof result.code, 'string');
+    assert(result.map);
+    result.map.file = path.basename(source_file.relative);
+    sourcemap.out(job, {
+      relative: source_file.relative,
+      contents: result.code,
+      map: result.map,
+      ...opts.sourcemap,
+    });
+    done();
+  }
+  return {
+    type: gb.SINGLE,
+    init: babelInit,
+    func: babelTaskFunc,
+  };
+};
--- build/index.js
+++ build/index.js
@@ -11,6 +11,7 @@
 const argv = require('minimist')(process.argv.slice(2));
 const assert = require('assert');
 const { asyncEachSeries } = require('glov-async');
+const babel = require('./babel.js');
 const bundle = require('./bundle.js');
 const compress = require('./compress.js');
 const gb = require('glov-build');
@@ -92,61 +93,6 @@ function copy(job, done) {
   done();
 }
 
-function babelTask(opts) {
-  opts = opts || {};
-  let babel_opts = {
-    sourceMap: true,
-    ...opts.babel,
-  };
-  let babel;
-  function babelInit(next) {
-    if (!babel) {
-      // eslint-disable-next-line global-require
-      babel = require('@babel/core');
-      // prime it for timing purposes
-      babel.transformSync('', {
-        filename: path.join(gb.getSourceRoot(), 'foo.js'),
-        filenameRelative: 'foo.js',
-        sourceFileName: 'foo.js',
-        ...babel_opts
-      });
-    }
-    next();
-  }
-  function babelTaskFunc(job, done) {
-    let source_file = job.getFile();
-    let source_code = source_file.contents.toString();
-    let result;
-    try {
-      result = babel.transformSync(source_code, {
-        // even if the file does not actually live in the source dir, treat it as such, for finding .babelrc files
-        filename: path.join(gb.getSourceRoot(), source_file.relative),
-        filenameRelative: source_file.relative,
-        sourceFileName: source_file.relative,
-        ...babel_opts
-      });
-    } catch (err) {
-      return void done(err);
-    }
-    assert.equal(typeof result.code, 'string');
-    assert(result.map);
-    result.map.file = path.basename(source_file.relative);
-    sourcemap.out(job, {
-      relative: source_file.relative,
-      contents: result.code,
-      map: result.map,
-      ...opts.sourcemap,
-    });
-    done();
-  }
-  return {
-    type: gb.SINGLE,
-    init: babelInit,
-    func: babelTaskFunc,
-  };
-}
-
-
 gb.task({
   name: 'client_static',
   input: config.client_static,
@@ -175,7 +121,7 @@ gb.task({
   name: 'server_js',
   input: config.server_js_files,
   target: 'dev',
-  ...babelTask({
+  ...babel({
     babel: {
       babelrc: false,
       presets: [['@babel/env', {
@@ -254,7 +200,7 @@ gb.task({
 gb.task({
   name: 'client_js_babel_files',
   input: config.client_js_files,
-  ...babelTask({
+  ...babel({
     sourcemap: {
       inline: true,
     },
