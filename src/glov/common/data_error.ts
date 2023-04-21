f0dc4b659f9e0c9ff6c9571305820ce410056481
Jimb Esser <wasteland@gmail.com>
2023-04-21 07:30:35 -0700

    data_error: Allow per-frame dataErrors for errors triggered every frame (will not fill buffer, will go away when a frame does not trigger it again)

--- src/glov/common/data_error.ts
+++ src/glov/common/data_error.ts
@@ -2,21 +2,33 @@ import assert from 'assert';
 
 export type DataError = {
   msg: string;
+  per_frame?: boolean;
 };
 
 let on_error: null | ((err: DataError) => void) = null;
 let enabled = false;
 let error_queue: DataError[] = [];
+let msgs_in_queue: Partial<Record<string, true>> = Object.create(null);
 export function dataErrorEx(err: DataError): void {
   if (!enabled) {
     return;
+  }
+  if (err.per_frame) {
+    if (msgs_in_queue[err.msg]) {
+      // Duplicate, silently ignore
+      return;
+    }
+    msgs_in_queue[err.msg] = true;
   }
   if (on_error) {
     on_error(err);
   }
   error_queue.push(err);
   if (error_queue.length > 25) {
-    error_queue.splice(0, 1);
+    let removed = error_queue.splice(0, 1)[0];
+    if (removed.per_frame) {
+      delete msgs_in_queue[removed.msg];
+    }
   }
 }
 
@@ -39,4 +51,5 @@ export function dataErrorQueueGet(): DataError[] {
 
 export function dataErrorQueueClear(): void {
   error_queue = [];
+  msgs_in_queue = Object.create(null);
 }
