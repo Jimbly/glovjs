0131871fc82e0f5475b15e9ea9b6f42c735db618
Jimb Esser <wasteland@gmail.com>
2023-09-12 13:20:55 -0700

    error_report: better reporting of uncaught promise rejections

--- src/glov/client/bootstrap.js
+++ src/glov/client/bootstrap.js
@@ -8,7 +8,7 @@ require('./polyfill.js');
 let debug = document.getElementById('debug');
 window.onerror = function (e, file, line, col, errorobj) {
   let msg = String(e);
-  if (msg === '[object Object]') {
+  if (msg.startsWith('[object ')) {
     try {
       msg = JSON.stringify(e);
     } catch (ignored) {
@@ -43,9 +43,23 @@ window.onerror = function (e, file, line, col, errorobj) {
   if (msg.indexOf('Error:') === -1) {
     msg = `Error: ${msg}`;
   }
-  if (errorobj && errorobj.type) {
-    if (errorobj.type === 'unhandledrejection') {
+  if (errorobj && errorobj.errortype) {
+    if (errorobj.errortype === 'unhandledrejection') {
       msg = `Uncaught (in promise) ${msg}`;
+    }
+  }
+  if (errorobj) {
+    // Attempt to tack on any string members of the error object that may contain useful details
+    try {
+      for (let key in errorobj) {
+        if (typeof errorobj[key] === 'string') {
+          if (key !== 'errortype') {
+            msg = `${msg}\n${key}=${errorobj[key]}`;
+          }
+        }
+      }
+    } catch (ignored) {
+      // ignored
     }
   }
   let show = true;
@@ -66,8 +80,16 @@ window.addEventListener('unhandledrejection', function (event) {
   if (!errorobj || typeof errorobj !== 'object') {
     errorobj = { stack: errorobj };
   }
-  errorobj.type = event.type;
-  window.onerror(event.reason, undefined, 0, 0, errorobj);
+  let file;
+  if (event.reason && event.reason.srcElement && event.reason.srcElement.src) {
+    file = event.reason.srcElement.src;
+  }
+  try {
+    errorobj.errortype = event.type;
+  } catch (ignored) {
+    // ignore, happens on Firefox
+  }
+  window.onerror(event.reason, file, 0, 0, errorobj);
 });
 window.debugmsg = function (msg, clear) {
   if (clear) {
