ae8ca5cf2cf691f322c151af3d4af6a85642b1df
Jimb Esser <wasteland@gmail.com>
2023-12-09 07:58:48 -0800

    auto_reset: split into autoResetSkippedFrames and autoResetEachFrame

--- src/glov/client/auto_reset.ts
+++ src/glov/client/auto_reset.ts
@@ -1,9 +1,18 @@
-import { TSMap } from 'glov/common/types';
+// eslint-disable-next-line @typescript-eslint/no-use-before-define
+export const autoReset = autoResetSkippedFrames;
+
 import * as engine from './engine';
+import type { TSMap } from 'glov/common/types';
 
 let auto_reset_data: TSMap<number> = Object.create(null);
-export function autoReset(key: string): boolean {
+export function autoResetSkippedFrames(key: string): boolean {
   let last_value: number | undefined = auto_reset_data[key];
   auto_reset_data[key] = engine.frame_index;
   return !(last_value! >= engine.frame_index - 1);
+}
+
+export function autoResetEachFrame(key: string): boolean {
+  let last_value: number | undefined = auto_reset_data[key];
+  auto_reset_data[key] = engine.frame_index;
+  return (last_value !== engine.frame_index);
 }
