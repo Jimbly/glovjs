e8e9a4ba8fce41dcc23576f8e6fb4dcb8c490f6e
Jimb Esser <wasteland@gmail.com>
2024-03-13 06:47:58 -0700

    global_worker and server_globals: allow registering client handlers for sysadmin direct actions

--- src/glov/server/global_worker.ts
+++ src/glov/server/global_worker.ts
@@ -1,7 +1,16 @@
 import assert from 'assert';
-import { CmdDef, HandlerSource, isClientHandlerSource } from 'glov/common/types';
+import {
+  CmdDef,
+  HandlerSource,
+  TSMap,
+  isClientHandlerSource,
+} from 'glov/common/types';
 import { ChannelServer } from './channel_server';
-import { ChannelWorker, HandleNewClientOpts } from './channel_worker';
+import {
+  ChannelWorker,
+  ClientHandlerFunction,
+  HandleNewClientOpts,
+} from './channel_worker';
 
 // General purpose worker(s) for handling global state
 
@@ -25,6 +34,7 @@ export class GlobalWorker extends ChannelWorker {
     return null;
   }
 }
+GlobalWorker.prototype.require_subscribe = false;
 GlobalWorker.prototype.auto_destroy = true;
 
 let global_worker_cmds: CmdDef[] = [];
@@ -37,6 +47,16 @@ export function globalWorkerAddCmd(cmd_def: CmdDef): void {
   global_worker_cmds.push(cmd_def);
 }
 
+let global_worker_client_handlers: TSMap<ClientHandlerFunction> = {};
+export function globalWorkerRegisterClientHandler<P=never, R=never>(
+  message: string,
+  handler: ClientHandlerFunction<P, R>
+): void {
+  assert(!inited);
+  assert(!global_worker_client_handlers[message]);
+  global_worker_client_handlers[message] = handler as unknown as ClientHandlerFunction;
+}
+
 export function globalWorkerInit(channel_server: ChannelServer): void {
   assert(!inited);
   inited = true;
@@ -44,5 +64,6 @@ export function globalWorkerInit(channel_server: ChannelServer): void {
     autocreate: true,
     subid_regex: /^(global)$/,
     cmds: global_worker_cmds,
+    client_handlers: global_worker_client_handlers,
   });
 }
