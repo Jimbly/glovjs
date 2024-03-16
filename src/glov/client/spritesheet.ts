36d01e6fa9f056190777a6b8f65285ef3b90378c
Jimb Esser <wasteland@gmail.com>
2024-03-16 11:52:49 -0700

    typescript: convert spritesheet.js to TypeScript

--- src/glov/client/spritesheet.ts
+++ src/glov/client/spritesheet.ts
@@ -1,28 +1,61 @@
-const assert = require('assert');
-const { vec2, vec4 } = require('glov/common/vmath.js');
-const { engineStartupFunc } = require('./engine.js');
-const { createSprite } = require('./sprites.js');
-const { textureLoad } = require('./textures.js');
+import assert from 'assert';
+import { TSMap } from 'glov/common/types';
+import {
+  Vec4,
+  vec2,
+  vec4,
+} from 'glov/common/vmath';
+import { engineStartupFunc } from './engine';
+import {
+  Sprite,
+  SpriteUIData,
+  Texture,
+  TextureOptions,
+  spriteCreate,
+} from './sprites';
+import { textureLoad } from './textures';
+
+export type SpriteSheetBuildTime = {
+  name: string;
+  tiles: TSMap<number>;
+  uidata: {
+    rects: Vec4[];
+    aspect?: number[];
+  };
+  layers?: number;
+  // [frame:string]: number; // FRAME_*
+};
+
+export type SpriteSheet = {
+  sprite: Sprite;
+  sprite_centered: Sprite;
+  sprite_centered_x: Sprite;
+  tiles: Partial<Record<string, number>>;
+};
+
 
 const uvs = vec4(0, 0, 1, 1);
 const origin_centered = vec2(0.5, 0.5);
 const origin_centered_x = vec2(0.5, 0);
-let load_opts = {};
+let load_opts: TSMap<TextureOptions> = {};
 let hit_startup = false;
-export function spritesheetTextureOpts(name, opts) {
+export function spritesheetTextureOpts(name: string, opts: TextureOptions): void {
   assert(!hit_startup);
   load_opts[name] = opts;
 }
-export function spritesheetRegister(runtime_data) {
+
+// Called only from auto-generated .js files
+export function spritesheetRegister(runtime_data_in: SpriteSheetBuildTime): void {
+  let runtime_data = runtime_data_in as (SpriteSheetBuildTime & SpriteSheet & TSMap<Sprite>);
   // Create with dummy data, will load later
-  let texs = [];
-  let sprite = runtime_data.sprite = createSprite({ texs, uvs });
+  let texs: Texture[] = [];
+  let sprite = runtime_data.sprite = spriteCreate({ texs, uvs });
   runtime_data[`sprite_${runtime_data.name}`] = sprite;
-  let sprite_centered = runtime_data.sprite_centered = createSprite({ texs, uvs, origin: origin_centered });
+  let sprite_centered = runtime_data.sprite_centered = spriteCreate({ texs, uvs, origin: origin_centered });
   runtime_data[`sprite_${runtime_data.name}_centered`] = sprite_centered;
-  let sprite_centered_x = runtime_data.sprite_centered_x = createSprite({ texs, uvs, origin: origin_centered_x });
+  let sprite_centered_x = runtime_data.sprite_centered_x = spriteCreate({ texs, uvs, origin: origin_centered_x });
   runtime_data[`sprite_${runtime_data.name}_centered_x`] = sprite_centered_x;
-  sprite.uidata = sprite_centered.uidata = sprite_centered_x.uidata = runtime_data.uidata;
+  sprite.uidata = sprite_centered.uidata = sprite_centered_x.uidata = runtime_data.uidata as SpriteUIData;
   engineStartupFunc(function () {
     hit_startup = true;
     let opts = load_opts[runtime_data.name] || {};
