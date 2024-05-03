dce4edda2aaa5ee7a072e59853e52c527c26914d
Jimb Esser <wasteland@gmail.com>
2024-05-03 15:50:35 -0700

    cmds: add /client_crash, /client_assert, /client_reject_now for error report testing

--- src/glov/client/cmds.js
+++ src/glov/client/cmds.js
@@ -1,6 +1,7 @@
 // Portions Copyright 2019 Jimb Esser (https://github.com/Jimbly/)
 // Released under MIT License: https://opensource.org/licenses/MIT
 
+import assert from 'assert';
 // Must have NO internal dependencies, otherwise get circular dependencies between this, engine, and settings
 import * as cmd_parse_mod from 'glov/common/cmd_parse';
 import * as local_storage from './local_storage';
@@ -87,5 +88,36 @@ cmd_parse.register({
       urlhash.go(str); // .goRoute(str) is also a reasonable implementation depending on the context
       profilerStop('/url');
     }, 1);
+  },
+});
+
+cmd_parse.register({
+  cmd: 'client_crash',
+  help: '(Debug) - Crash on the client',
+  access_show: ['sysadmin'],
+  func: function (str, resp_func) {
+    let foo;
+    foo.bar++;
+  }
+});
+
+cmd_parse.register({
+  cmd: 'client_assert',
+  help: '(Debug) - Fail an assert on the client',
+  access_show: ['sysadmin'],
+  func: function (str, resp_func) {
+    assert(false);
+  },
+});
+
+cmd_parse.register({
+  cmd: 'client_reject_now',
+  help: '(Debug) - Fail an unhandled promise rejection on the client (Error, sync)',
+  access_show: ['sysadmin'],
+  func: function (str, resp_func) {
+    // eslint-disable-next-line no-new
+    new Promise((resolve, reject) => {
+      reject(new Error('client_reject_now'));
+    });
   },
 });
