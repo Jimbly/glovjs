27636c8dbdfb2b1eaa2114280b5fc8526bac3f19
Jimb Esser <wasteland@gmail.com>
2024-10-07 14:07:00 -0700

    asset_hashing: fix loading _old_ worker bundle when reloading in development

--- src/glov/client/locate_asset.ts
+++ src/glov/client/locate_asset.ts
@@ -1,9 +1,31 @@
+/* eslint-env browser */
 import type { DataObject, TSMap } from 'glov/common/types';
 
 let asset_mappings = typeof window === 'undefined' ?
   {} :
   (window as unknown as DataObject).glov_asset_mappings as TSMap<string>;
 let asset_dir = asset_mappings && asset_mappings.asset_dir || '';
+
+// Raw LocalStorage access, not using the module, since this is imported
+//   _before_ we've had a chance to set our local storage key.
+let lsd = (function (): Partial<Record<string, string>> {
+  if (typeof window === 'undefined') {
+    return {};
+  }
+  try {
+    localStorage.setItem('test', 'test');
+    localStorage.removeItem('test');
+    return localStorage;
+  } catch (e) {
+    return {};
+  }
+}());
+
+const DISABLED_KEY = 'asset_hashing_disabled_until';
+if (Number(lsd[DISABLED_KEY]) > Date.now()) {
+  console.log('Disabling asset mappings due to auto-reload.');
+  asset_mappings = {};
+}
 
 let proxy_path: string = '';
 // e.g. `.proxy/`
@@ -54,6 +76,9 @@ export function locateAsset(name: string): string {
 // Called in development before doing any reloads (as we don't reload asset_mappings)
 export function locateAssetDisableHashing(): void {
   asset_mappings = {};
+  // Also, if a reload is triggered after this, disable asset hashing in that
+  //   session, as the reload happens _before_ the new assets.js is written out.
+  lsd[DISABLED_KEY] = String(Date.now() + 5000);
 }
 
 export function unlocatePaths(s: string | null | undefined): string {
