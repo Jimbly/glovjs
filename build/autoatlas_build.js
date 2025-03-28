5ac99ea493d43cfbb4774568643075b583adf1d6
Jimb Esser <wasteland@gmail.com>
2025-03-28 15:35:39 -0700

    autoatlas: fix incorrect pixels when clamping

--- build/autoatlas_build.js
+++ build/autoatlas_build.js
@@ -288,7 +288,7 @@ module.exports = function () {
             for (let xx = -pad; xx < imgw + pad; ++xx) {
               let xxx;
               if (clamp_horiz) {
-                xxx = xx < 0 ? 0 : xx >= imgw ? imgh - 1 : xx;
+                xxx = xx < 0 ? 0 : xx >= imgw ? imgw - 1 : xx;
               } else {
                 xxx = (xx + imgw) % imgw;
               }
