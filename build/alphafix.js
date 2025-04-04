53201f94c83e4645862b1dc89cbaba8bdcd96610
Jimb Esser <wasteland@gmail.com>
2025-04-04 06:55:06 -0700

    build: greatly improve performance of client_png/alphafix build task on large images with lots of transparency

--- build/alphafix.js
+++ build/alphafix.js
@@ -32,74 +32,97 @@ module.exports = function (globs) {
     assert.equal(width * height * 4, data.length);
     let dim = width * height;
     let is_solid = new Uint8Array(dim);
-    let todo = [];
-    for (let ii = 0; ii < dim; ++ii) {
-      if (data[ii*4 + 3]) {
-        is_solid[ii] = 1;
-      } else {
-        todo.push(ii);
+    let queued = new Uint8Array(dim);
+    let todo_buf = new Uint32Array(dim);
+    let todo_start = 0;
+    let todo_end = 0;
+    function addNeighbors(idx) {
+      let x = idx % width;
+      if (x > 0 && !queued[idx - 1]) {
+        todo_buf[todo_end++] = idx - 1;
+        queued[idx - 1] = 1;
+      }
+      if (x + 1 < width && !queued[idx + 1]) {
+        todo_buf[todo_end++] = idx + 1;
+        queued[idx + 1] = 1;
+      }
+      if (idx > width && !queued[idx - width]) {
+        todo_buf[todo_end++] = idx - width;
+        queued[idx - width] = 1;
+      }
+      if (idx + width < dim && !queued[idx + width]) {
+        todo_buf[todo_end++] = idx + width;
+        queued[idx + width] = 1;
       }
     }
-    if (!todo.length) {
+    for (let idx = 0; idx < dim; ++idx) {
+      if (data[idx*4 + 3]) {
+        queued[idx] = 1;
+        is_solid[idx] = 1;
+        addNeighbors(idx);
+      }
+    }
+    if (!todo_end) {
       job.out(file);
       return void done();
     }
-    let diff = 0;
-    let did_anything = true;
-    while (did_anything) {
-      did_anything = false;
-      let done = [];
-      for (let ii = todo.length - 1; ii >= 0; --ii) {
-        let idx = todo[ii];
-        let y = floor(idx / width);
-        let x = idx - y * width;
-        let c = 0;
-        let r = 0;
-        let g = 0;
-        let b = 0;
-        if (x > 0 && is_solid[idx - 1]) {
-          r += data[idx*4-4];
-          g += data[idx*4-3];
-          b += data[idx*4-2];
-          c++;
+    let diff = false;
+    let solid_mark = [];
+    let loop_end = todo_end;
+    while (todo_start < todo_end) {
+      if (todo_start === loop_end) {
+        for (let ii = 0; ii < solid_mark.length; ++ii) {
+          is_solid[solid_mark[ii]] = 1;
         }
-        if (x < width-1 && is_solid[idx + 1]) {
-          r += data[idx*4+4];
-          g += data[idx*4+5];
-          b += data[idx*4+6];
-          c++;
-        }
-        if (y > 0 && is_solid[idx - width]) {
-          r += data[(idx-width)*4];
-          g += data[(idx-width)*4+1];
-          b += data[(idx-width)*4+2];
-          c++;
-        }
-        if (y < height-1 && is_solid[idx + width]) {
-          r += data[(idx+width)*4];
-          g += data[(idx+width)*4+1];
-          b += data[(idx+width)*4+2];
-          c++;
-        }
-        if (c) {
-          did_anything = true;
-          todo[ii] = todo[todo.length - 1];
-          todo.pop();
-          done.push(idx);
-          r = round(r/c);
-          g = round(g/c);
-          b = round(b/c);
-          diff += abs(data[idx*4] - r);
-          diff += abs(data[idx*4+1] - g);
-          diff += abs(data[idx*4+2] - b);
-          data[idx*4] = r;
-          data[idx*4+1] = g;
-          data[idx*4+2] = b;
-        }
+        solid_mark.length = 0;
+        loop_end = todo_end;
       }
-      for (let ii = 0; ii < done.length; ++ii) {
-        is_solid[done[ii]] = 1;
+      let idx = todo_buf[todo_start++];
+      if (is_solid[idx]) {
+        continue;
       }
+      let y = floor(idx / width);
+      let x = idx - y * width;
+      let c = 0;
+      let r = 0;
+      let g = 0;
+      let b = 0;
+      if (x > 0 && is_solid[idx - 1]) {
+        r += data[idx*4-4];
+        g += data[idx*4-3];
+        b += data[idx*4-2];
+        c++;
+      }
+      if (x < width-1 && is_solid[idx + 1]) {
+        r += data[idx*4+4];
+        g += data[idx*4+5];
+        b += data[idx*4+6];
+        c++;
+      }
+      if (y > 0 && is_solid[idx - width]) {
+        r += data[(idx-width)*4];
+        g += data[(idx-width)*4+1];
+        b += data[(idx-width)*4+2];
+        c++;
+      }
+      if (y < height-1 && is_solid[idx + width]) {
+        r += data[(idx+width)*4];
+        g += data[(idx+width)*4+1];
+        b += data[(idx+width)*4+2];
+        c++;
+      }
+      assert(c);
+      r = round(r/c);
+      g = round(g/c);
+      b = round(b/c);
+      diff ||= data[idx*4] !== r;
+      diff ||= data[idx*4+1] !== g;
+      diff ||= data[idx*4+2] !== b;
+      data[idx*4] = r;
+      data[idx*4+1] = g;
+      data[idx*4+2] = b;
+      addNeighbors(idx);
+      solid_mark.push(idx);
     }
 
     let buffer = pngWrite(pngin);
