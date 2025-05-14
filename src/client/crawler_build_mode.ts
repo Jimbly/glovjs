d6c5c49e2def1ff24555d01ba2a2c1fa978bdf59
Jimb Esser <wasteland@gmail.com>
2025-05-14 16:20:51 -0700

    Use long names for tabs on non-pixely fonts

--- src/client/crawler_build_mode.ts
+++ src/client/crawler_build_mode.ts
@@ -124,6 +124,12 @@ enum BuildTab {
   Config = 'Cnfg',
 }
 
+const BUILD_TAB_LONG = {
+  [BuildTab.Paint]: 'Paint',
+  [BuildTab.Path]: 'Path',
+  [BuildTab.Config]: 'Config',
+};
+
 function validBuildTab(str: string | undefined): BuildTab | null {
   for (let key in BuildTab) {
     if (BuildTab[key as keyof typeof BuildTab] === str) {
@@ -1487,7 +1493,7 @@ export function crawlerBuildModeUI(frame: Box & { map_view: boolean }): void {
       h: button_height,
       disabled: build_tab === tab,
       base_name: build_tab === tab ? 'buttonselected' : 'button',
-      text: tab,
+      text: font.integral ? tab : BUILD_TAB_LONG[tab],
       hotkey: show_palette_config && build_tab === BuildTab.Paint ? undefined :
         keyDown(KEYS.ALT) ? KEYS['1'] + idx : undefined,
       font,
