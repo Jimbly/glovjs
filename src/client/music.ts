a339a0400a41628c81dbbd7fadfb82d347594f4e
Jimb Esser <wasteland@gmail.com>
2026-03-23 08:59:34 -0700

    Allow hard-coded music volume scale

--- src/client/music.ts
+++ src/client/music.ts
@@ -4,15 +4,16 @@ import * as settings from 'glov/client/settings';
 import { GlovSoundSetUp, soundLoad, soundPlay, soundResumed } from 'glov/client/sound';
 import type { TSMap } from 'glov/common/types';
 
+const MUSIC_VOLUME = 1;
 let last_music_ref: GlovSoundSetUp | null;
 let last_music_name: string | null = null;
 let last_music_fading_out = false;
 let playing_music_name: string | null = null;
 let loading_music: TSMap<true> = {};
 let loaded_music: TSMap<true> = {};
-const FADE_OUT_CHANGE = 500;
-const FADE_OUT_SILENCE = 5000;
-const FADE_UP = 2500;
+const FADE_OUT_CHANGE = 500/MUSIC_VOLUME;
+const FADE_OUT_SILENCE = 5000/MUSIC_VOLUME;
+const FADE_UP = 2500/MUSIC_VOLUME;
 export function tickMusic(music_name: string | null): void {
   // if (!music_name && optionsMenuVisible()) {
   //   music_name = 'music_menu';
@@ -38,7 +39,7 @@ export function tickMusic(music_name: string | null): void {
         assert(playing_music_name === null);
         assert(last_music_fading_out);
         // already playing the right music, it's just fading out
-        last_music_ref.fade(1, FADE_UP);
+        last_music_ref.fade(MUSIC_VOLUME, FADE_UP);
         playing_music_name = last_music_name;
         last_music_fading_out = false;
       } else {
@@ -56,12 +57,12 @@ export function tickMusic(music_name: string | null): void {
     if (!last_music_ref) { // finished fading out, can start something new
       if (music_name && loaded_music[music_name]) {
         last_music_ref = soundPlay(music_name, {
-          volume: 0.01,
+          volume: 0.001,
           as_music: true,
         });
         last_music_fading_out = false;
         if (last_music_ref) {
-          last_music_ref.fade(1, FADE_UP);
+          last_music_ref.fade(MUSIC_VOLUME, FADE_UP);
           last_music_name = music_name;
           playing_music_name = music_name;
         }
